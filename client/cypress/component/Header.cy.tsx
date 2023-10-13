1b34d082da2e66e7d192a2bb380f7682797afc91
KennyTaiDuong <kennyduong537@gmail.com>
2023-10-13 13:00:31 -0500

    successfully created and rendered basic header component

---
+++ client/cypress/component/Header.cy.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../../src/components/Header/Header"
+
+describe('Header component', () => {
+  it('should render Header', () => {
+    cy.mount(<Header />)
+  })
+
+
+})
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  background-color: rgba(188,50,48,255);
+`
+
+export const Header = () => {
+  return (
+    <Container>
+      Header
+    </Container>
+  )
+}
