c2494b60da4f2cc1f1d17c87574a53a1533fb72d
KennyTaiDuong <kennyduong537@gmail.com>
2023-10-19 08:38:20 -0500

    corrected tests for header

--- client/cypress/component/Layout.cy.tsx
+++ client/cypress/component/Layout.cy.tsx
@@ -5,15 +5,31 @@ import Global from "../../src/GlobalStyles"
 const MockLayout = () => {
   return (
     <BrowserRouter>
-    <Global />
+      <Global />
       <Layout />
     </BrowserRouter>
   )
 }
 
 describe("Testing functionality of layour", () => {
-  it("should render component", async () => {
+  beforeEach(() => {
     cy.viewport('iphone-x')
     cy.mount(<MockLayout />)
+  })
+
+  it("should render component", () => {
+    cy.get('[data-cy="hamburger-icon"]').click()
+    
+  })
+
+  it('should display search bar when icon clicked', () => {
+    cy.get('[data-cy="search-button"]').click()
+    cy.get(`[data-cy="search-bar"]`).should('have.css', 'display').and('match', /block/)
+  })
+
+  it('should close search bar when icon clicked twice', () => {
+    cy.get('[data-cy="search-button"]').click()
+    cy.get('[data-cy="search-button"]').click()
+    cy.get(`[data-cy="search-bar"]`).should('have.css', 'display').and('match', /none/)
   })
 })
