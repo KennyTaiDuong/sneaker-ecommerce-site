9dbbd10bea8bf4a5a0f8ea632fa5a48a430e5463
KennyTaiDuong <kennyduong537@gmail.com>
2023-10-19 22:40:15 -0500

    "added cors to server"

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const productRoutes = require("./products/route");
 const userRoutes = require("./users/route");
 const orderRoutes = require("./orders/route");
 const cartRoutes = require("./carts/route");
+const cors = require("cors");
 
 const express = require("express");
 
@@ -15,6 +16,14 @@ app.use(express.json());
 app.listen(process.env.PORT, () => {
   console.log(`Connected to db and listening on port ${process.env.PORT}`);
 });
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
