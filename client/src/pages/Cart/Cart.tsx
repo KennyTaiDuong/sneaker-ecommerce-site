e865c50fdae1a848cd822aaceb9ca3ad8c0a413f
KennyTaiDuong <kennyduong537@gmail.com>
2023-11-06 12:40:30 -0600

    update each cart item with remove button to take out of cart

--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -11,7 +11,6 @@ const Container = styled.div`
 const Username = styled.p`
   font-size: 1.5rem;
   font-weight: 700;
-
 `
 
 const ReceiptContainer = styled.div`
@@ -61,20 +60,32 @@ const CategoryLabel = styled.p`
 
 const SizeLabel = styled(CategoryLabel)`
   grid-column: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const QuantityLabel = styled(CategoryLabel)`
   grid-column: 4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const PriceLabel = styled(CategoryLabel)`
   grid-column: 5;
   border-right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const ItemLabel = styled.p`
   padding: 0.25rem;
   border-right: 1px solid rgb(138,134,85);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 const ItemNumber = styled.p`
@@ -83,9 +94,17 @@ const ItemNumber = styled.p`
   text-align: center;
 `
 
+const RemoveButton = styled.button`
+  background-color: transparent;
+  border: 0;
+  font-size: 0.5rem;
+  color: rgb(138,134,85);
+  padding: 0;
+`
+
 export const Cart = () => {
   const { isLoading, isAuthenticated, user } = useAuth0() 
-  const {currentCart, currentUser} = useContext(UserDataContext)
+  const {currentCart, currentUser, setCurrentCart} = useContext(UserDataContext)
   const navigate = useNavigate()
 
   const [totalPrice, setTotalPrice] = useState(0)
@@ -96,14 +115,19 @@ export const Cart = () => {
   function getTotalPrice() {
     
     const totalArray = currentCart?.products?.map((shoe) => {
+      console.log(shoe)
       return shoe.price
     })
 
-    const total = totalArray?.reduce((acc, current) => {
-      return acc + current
-    })
-
-    setTotalPrice(total ? total : 0)
+    if (!totalArray) {
+      const total = totalArray?.reduce((acc: any, current: any) => {
+        return acc + current
+      })
+  
+      setTotalPrice(total)
+    } else {
+      setTotalPrice(0)
+    }
   }
 
   function getTotalQuantity() {
@@ -120,9 +144,43 @@ export const Cart = () => {
   }
 
   getTotalPrice()
-  getTotalQuantity()
+  // getTotalQuantity()
 
   }, [currentCart])
+
+  async function RemoveItem(itemIndex: number) {
+    const updatedCart = currentCart?.products?.filter((_item, index) => {
+      return index !== itemIndex
+    })
+
+    try {
+      await fetch(`http://localhost:5000/api/carts/${currentUser?.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          products: updatedCart
+        })
+      })
+
+      fetchCart()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function fetchCart() {
+    try {
+      const res = await fetch(`http://localhost:5000/api/carts/${currentUser?.id}`)
+
+      const data = await res.json()
+
+      setCurrentCart(data.rows[0])
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   const itemsElements = currentCart?.products?.map((item, index) => {
     const { name, price, quantity, size } = item
@@ -130,7 +188,10 @@ export const Cart = () => {
     return (
       <ReceiptRow key={index}>
         <ItemNumber>{index + 1}</ItemNumber>
-        <ItemLabel>{name}</ItemLabel>
+        <ItemLabel>
+          {name}
+          <RemoveButton onClick={() => RemoveItem(index)}>remove</RemoveButton>
+        </ItemLabel>
         <SizeLabel>{size}</SizeLabel>
         <QuantityLabel>{quantity}</QuantityLabel>
         <PriceLabel>${price}</PriceLabel>
