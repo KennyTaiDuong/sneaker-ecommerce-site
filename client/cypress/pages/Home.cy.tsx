fad35051fafbab6c4aaf1d00a41b7a407a27b2ce
KennyTaiDuong <kennyduong537@gmail.com>
2023-11-22 09:02:29 -0600

    fixed home page test to handle fetch error

--- client/cypress/pages/Home.cy.tsx
+++ client/cypress/pages/Home.cy.tsx
@@ -14,6 +14,8 @@ const MockHome = () => {
 describe("Home page", () => {
   
   it("handle failed fetch request", () => {
+    cy.viewport("iphone-x")
+    cy.mount(<MockHome />)
     cy.intercept("GET", "http://localhost:5000/api/products", {
       statusCode: 400,
       body: "Something went wrong",
@@ -23,7 +25,7 @@ describe("Home page", () => {
     }).as("fetchProducts")
   })
 
-  it("should mount home page", () => {
+  it("should mount home page and render products", () => {
     cy.viewport("iphone-x")
     cy.mount(<MockHome />)
   })
