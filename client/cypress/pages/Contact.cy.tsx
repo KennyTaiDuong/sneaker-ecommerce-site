aa206cf91977d98e4a3b7736719d11d026c13797
KennyTaiDuong <kennyduong537@gmail.com>
2023-11-22 14:47:49 -0600

    added tests for products page

--- client/cypress/pages/Contact.cy.tsx
+++ client/cypress/pages/Contact.cy.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Contact } from "../../src/pages/Contact";
+import Global from "../../src/GlobalStyles";
 
 const MockContact = () => {
   return (
     <BrowserRouter>
+      <Global />
       <Contact />
     </BrowserRouter>
   )
--- client/cypress/pages/Products/Products.cy.tsx
+++ client/cypress/pages/Products/Products.cy.tsx
@@ -1,69 +1,117 @@
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { Products } from "../../../src/pages/Product/Products";
 import Global from "../../../src/GlobalStyles";
+import { ProductsContext } from "../../../src/components/Layout/ProductsLayout";
 
-const MockProducts = () => {
+type Size = {
+  size: string,
+  quantity: string
+}
+
+type Product = {
+  sku: string;
+  name: string;
+  price: number;
+  sizes: Size[];
+  category: "m" | "w" | "gs" | "ps" | "td" | "yeezy" | "slide" | "foam";
+  images: string;
+}
+
+type Props = {
+  searchQuery: string
+}
+
+const MockProducts = ({ searchQuery }: Props) => {
+  const allProducts: Product[] = [
+    {
+      sku: "CU1726-100",
+      name: 'Nike Dunk Low "Kentucky"',
+      price: 300,
+      sizes: [
+        {
+          quantity: "3",
+          size: "8"
+        },
+        {
+          quantity: "2",
+          size: "8.5"
+        }
+      ],
+      category: "m",
+      images: "https://image.goat.com/attachments/product_template_additional_pictures/images/079/877/500/medium/603667_03.jpg.jpeg?1666623686"
+    },
+    {
+      sku: "DD1391-100",
+      name: 'Nike Dunk Low "Panda"',
+      price: 200,
+      sizes: [
+        {
+          quantity: "3",
+          size: "8"
+        },
+        {
+          quantity: "2",
+          size: "8.5"
+        }
+      ],
+      category: "m",
+      images: "https://www.kicksonfire.com/wp-content/uploads/2022/01/Nike-Dunk-Low-Panda-DD1391-100-1.jpeg"
+    },
+    {
+      sku: "DH6927-111",
+      name: 'Jordan 4 "Military Black"',
+      price: 500,
+      sizes: [
+        {
+          quantity: "3",
+          size: "8"
+        },
+        {
+          quantity: "2",
+          size: "8.5"
+        }
+      ],
+      category: "m",
+      images: "https://2app.kicksonfire.com/kofapp/upload/events_master_images/ipad_air-jordan-4-military-black.jpeg"
+    }
+  ]
+
   return (
-    <BrowserRouter>
+    <MemoryRouter initialEntries={[searchQuery]}>
       <Global />
-      <Products />
-    </BrowserRouter>
+      <ProductsContext.Provider value={{ allProducts }}>
+        <Products />
+      </ProductsContext.Provider>
+    </MemoryRouter>
   )
 }
 
 describe("Products Page", () => {
-  beforeEach(() => {
-    cy.viewport("iphone-x")
-    cy.intercept("GET", "http://localhost:5000/api/products", {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: [
-        {
-          sku: "DD1391-100",
-          name: 'Nike Dunk Low "Panda"',
-          category: "m",
-          sizes: [],
-          price: 250,
-          images: "https://www.kicksonfire.com/wp-content/uploads/2022/01/Nike-Dunk-Low-Panda-DD1391-100-1.jpeg"
-        }
-      ],
-    })
-    cy.mount(<MockProducts />)
-  })
-
-  it("handle failed fetch request", () => {
-    cy.intercept("GET", "http://localhost:5000/api/products", {
-      statusCode: 400,
-      body: "Something went wrong",
-      headers: {
-        'content-type': 'application/json',
-      },
-    }).as("fetchProducts")
-  })
 
   it("should render empty page", () => {
-
+    cy.mount(<MockProducts searchQuery="/?page=1&query=lsod"/>)
+    cy.contains("No products found!").should("be.visible")
   })
 
-  // it("sort by lowest price", () => {
-  //   cy.get("[data-cy='sort-btn']").click()
-  //   cy.get("[data-cy='sort-low']").click()
-  // })
+  it("should render first page with all test products", () => {
+    cy.mount(<MockProducts searchQuery="/?page=1"/>)
+    cy.get(`[data-cy="button-0"]`).should("be.visible")
+    cy.contains("Kentucky").should("be.visible")
+    cy.contains("Panda").should("be.visible")
+  })
 
-  // it("sort by highest price", () => {
-  //   cy.get("[data-cy='sort-btn']").click()
-  //   cy.get("[data-cy='sort-high']").click()
-  // })
+  it("should render only brand passed in search query", () => {
+    cy.mount(<MockProducts searchQuery="/?page=1&brand=jordan"/>)
+    cy.contains("Jordan 4 \"Military Black\"").should("be.visible")
+  })
 
-  // it("sort by alphabetical order", () => {
-  //   cy.get("[data-cy='sort-btn']").click()
-  //   cy.get("[data-cy='sort-alpha']").click()
-  // })
+  it("should render products that match query", () => {
+    cy.mount(<MockProducts searchQuery="/?page=1&query=dunk"/>)
+    cy.contains("Kentucky").should("be.visible")
+  })
 
-  // it("sort by reverse-alphabetical order", () => {
-  //   cy.get("[data-cy='sort-btn']").click()
-  //   cy.get("[data-cy='sort-reverse']").click()
-  // })
+  it("should render sidebar in tablet/bigger screen size", () => {
+    cy.mount(<MockProducts searchQuery="/?page=1&query=dunk"/>)
+    cy.get('[data-cy="close-button"]')
+  })
 })
